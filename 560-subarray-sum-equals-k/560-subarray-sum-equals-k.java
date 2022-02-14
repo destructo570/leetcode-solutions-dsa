a606b7eb7410f1ecc5a71186361e730b1c9a72b6
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-02-14 10:53:37 +0530

    Time: 1361 ms (13.44%), Space: 55.4 MB (57.57%) - LeetHub

--- 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.java
@@ -1,6 +1,32 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
         
+       return naive(nums, k);
+    }
+    
+    public int optimal(int[] nums, int k){
+        int count=0;
+        int curSum=0;
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        
+        for(int i=0; i<nums.length; i++){
+            curSum=curSum+nums[i];
+            
+            if(curSum==k){
+                count++;
+            }
+            
+            if(hs.containsKey(curSum-k)){
+                count++;
+            }
+            
+            hs.put(curSum, i);
+        }
+        
+        return count;
+    }
+    
+    public int naive(int[] nums, int k){
         int count=0;
         for(int i=0; i<nums.length; i++){
             long sum=0;
