978dd5995b46b9719a3d42226617e4fb607bd1d9
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-01 00:31:29 +0530

    Time: 3 ms (76.39%), Space: 43.4 MB (30.54%) - LeetHub

--- 415-add-strings/415-add-strings.java
+++ 415-add-strings/415-add-strings.java
@@ -12,30 +12,24 @@ class Solution {
         int j=num2.length()-1;
         int carry=0;
         while(j>=0){
-            int res=0;
-            int n1 = (int)num1.charAt(i)-48;
-            int n2 = (int)num2.charAt(j)-48;
-            if(n1+n2+carry >=10){
-                res=(n1+n2+carry)%10;
+            int res=((int)num1.charAt(i)-48)+((int)num2.charAt(j)-48)+carry;
+            if(res >=10){
+                res=res%10;
                 carry=1;
             }else {
-                res=n1+n2+carry;
                 carry=0;
             }
-
             result.append(res);
             i--;
             j--;
         }
 
         while (i>=0){
-            int res=0;
-            int n1 = (int)num1.charAt(i)-48;
-            if(carry>0 && n1+carry>=10){
-                res=(n1+carry)%10;
+            int res=((int)num1.charAt(i)-48)+carry;
+            if(carry>0 && res>=10){
+                res=res%10;
                 carry=1;
             }else {
-                res=n1+carry;
                 carry=0;
             }
             result.append(res);
