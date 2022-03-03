d5a8adf6d6780efbfbbc6dae37f9a66b4ad75e87
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-03 20:49:08 +0530

    Time: 0 ms (100.00%), Space: 41.9 MB (37.11%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -1,52 +1,45 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int col = matrix[0].length;
+      int col = matrix[0].length;
         int row = matrix.length;
         int TopRight = 0;
         int BottomLeft = 0;
         int TopBottom = 1;
         int BottomTop = 1;
-        int count = 0;
 
         ArrayList<Integer> result = new ArrayList<>();
 
         int k=0;
-        int l=0;
 
-        while (count!=(col*row)){
-            for(int j=TopRight; j<matrix[0].length-TopRight; j++){
-                if(count!=(col*row)){
+        while (result.size()!=(col*row)){
+            for(int j=TopRight; j<col-TopRight; j++){
+                if(result.size()!=(col*row)){
                     result.add(matrix[k][j]);
-                    count++;
                 }else break;
             }
             TopRight++;
 
             for(int j=TopBottom; j<row-TopBottom; j++){
-                if(count!=(col*row)){
-                    result.add(matrix[j][(col-1)-l]);
-                    count++;
+                if(result.size()!=(col*row)){
+                    result.add(matrix[j][(col-1)-k]);
                 }else break;
             }
             TopBottom++;
 
-            for(int j=(col-1)-BottomLeft; j>=BottomLeft; j--){
-                if(count!=(col*row)){
+            for(int j=(col-BottomLeft)-1; j>=BottomLeft; j--){
+                if(result.size()!=(col*row)){
                     result.add(matrix[(row-1)-k][j]);
-                    count++;
                 }else break;
             }
             BottomLeft++;
 
             for(int j=(row-BottomTop)-1; j>=BottomTop; j--){
-                if(count!=(col*row)){
-                    result.add(matrix[j][l]);
-                    count++;
+                if(result.size()!=(col*row)){
+                    result.add(matrix[j][k]);
                 }else break;
             }
             BottomTop++;
 
-            l++;
             k++;
         }
 
