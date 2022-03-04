c1163361c2d1c2a5ff79f6b2b71aecea8ab151cd
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-04 09:50:40 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (11.23%) - LeetHub

--- 54-spiral-matrix/54-spiral-matrix.java
+++ 54-spiral-matrix/54-spiral-matrix.java
@@ -6,37 +6,31 @@ class Solution {
         int BottomLeft = 0;
         int TopBottom = 1;
         int BottomTop = 1;
+        int elements = col*row;
 
         ArrayList<Integer> result = new ArrayList<>();
 
         int k=0;
 
-        while (result.size()!=(col*row)){
-            for(int j=TopRight; j<col-TopRight; j++){
-                if(result.size()!=(col*row)){
-                    result.add(matrix[k][j]);
-                }else break;
+        while (result.size() < elements){
+            for(int j=TopRight; j<col-TopRight && result.size() < elements; j++){
+                result.add(matrix[k][j]);
             }
             TopRight++;
 
-            for(int j=TopBottom; j<row-TopBottom; j++){
-                if(result.size()!=(col*row)){
-                    result.add(matrix[j][(col-1)-k]);
-                }else break;
+            for(int j=TopBottom; j<row-TopBottom && result.size() < elements; j++){
+                result.add(matrix[j][(col-1)-k]);
             }
             TopBottom++;
 
-            for(int j=(col-BottomLeft)-1; j>=BottomLeft; j--){
-                if(result.size()!=(col*row)){
-                    result.add(matrix[(row-1)-k][j]);
-                }else break;
+            for(int j=(col-BottomLeft)-1; j>=BottomLeft && result.size() < elements; j--){
+                result.add(matrix[(row-1)-k][j]);
+          
             }
             BottomLeft++;
 
-            for(int j=(row-BottomTop)-1; j>=BottomTop; j--){
-                if(result.size()!=(col*row)){
-                    result.add(matrix[j][k]);
-                }else break;
+            for(int j=(row-BottomTop)-1; j>=BottomTop && result.size() < elements; j--){
+                result.add(matrix[j][k]);
             }
             BottomTop++;
 
