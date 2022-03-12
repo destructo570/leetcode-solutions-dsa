5526d98c4b7c22771243bf62afe47d6c416fe083
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-12 09:02:06 +0530

    Time: 1 ms (74.85%), Space: 51.8 MB (14.27%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,6 +1,25 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
+        int sum=0;
+        int actualSum=0;
+        
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+        }
+        
+        for(int i=0; i<=nums.length; i++){
+            actualSum+=i;
+        }
+        
+        return actualSum-sum;
+    
+    }
+        
+    
+        
+/*    public int missingNumberHashing(int[] nums) {
+        
         int[] hs = new int[nums.length+1];
         
         for(int i=0; i<nums.length; i++){
@@ -16,4 +35,6 @@ class Solution {
         return -1;
         
     }
+*/
 }
+
