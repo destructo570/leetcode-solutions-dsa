479adc015e95dcbe5b3e1b25712a320333ae9395
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-12 14:14:01 +0530

    Time: 0 ms (100.00%), Space: 40.8 MB (58.69%) - LeetHub

--- 75-sort-colors/75-sort-colors.java
+++ 75-sort-colors/75-sort-colors.java
@@ -1,11 +1,41 @@
 class Solution {
     public void sortColors(int[] nums) {
         
-        int[] res = mergeSort(nums, 0, nums.length-1);
-        for(int i=0; i<nums.length; i++){
-            nums[i]=res[i];
-        }
+        // int[] res = mergeSort(nums, 0, nums.length-1);
+        // for(int i=0; i<nums.length; i++){
+        //     nums[i]=res[i];
+        // }
+        sortColorsTwo(nums);
         
+    }
+    
+    public void sortColorsTwo(int[] arr){
+        int zero=0; int one=0; int two=0;
+        
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==0){
+                zero++;
+            }else if(arr[i]==1){
+                one++;
+            }else{
+                two++;
+            }
+        }
+
+        for(int i=0; i<arr.length; i++){
+            if(zero!=0){
+                arr[i]=0;
+                zero--;
+                
+            }else if(one!=0){
+                arr[i]=1;
+                one--;
+              
+            }else{
+                arr[i]=2;
+                two--;
+            }
+        }
     }
     
     public int[] mergeSort(int[] arr,int l,int r){
