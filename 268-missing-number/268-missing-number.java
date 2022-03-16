4ed43490fa784f992aca2289da96fa6f4071df85
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-16 23:02:59 +0530

    Time: 0 ms (100.00%), Space: 50.8 MB (56.87%) - LeetHub

--- 268-missing-number/268-missing-number.java
+++ 268-missing-number/268-missing-number.java
@@ -1,6 +1,35 @@
 class Solution {
     public int missingNumber(int[] nums) {
         
+    int xor = 0, i = 0;
+	for (i = 0; i < nums.length; i++) {
+		xor = xor ^ i ^ nums[i];
+	}
+
+	return xor ^ i;
+    
+    }
+    
+/*    
+    public int missingNumberSimpleSum(int[] nums) {
+        
+        int sum=0;
+        int actualSum=0;
+        
+        for(int i=0; i<nums.length; i++){
+            sum+=nums[i];
+            actualSum+=i+1;
+        }
+        
+        return actualSum-sum;
+    
+    }
+*/
+        
+    
+        
+/*    public int missingNumberHashing(int[] nums) {
+        
         int[] hs = new int[nums.length+1];
         
         for(int i=0; i<nums.length; i++){
@@ -16,4 +45,6 @@ class Solution {
         return -1;
         
     }
+*/
 }
+
