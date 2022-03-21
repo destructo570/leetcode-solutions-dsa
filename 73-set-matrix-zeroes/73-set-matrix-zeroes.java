33d27788f33fe3da3d4f6926045ca2d78dedbefd
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-21 22:43:27 +0530

    Time: 0 ms (100.00%), Space: 43.4 MB (96.24%) - LeetHub

--- 73-set-matrix-zeroes/73-set-matrix-zeroes.java
+++ 73-set-matrix-zeroes/73-set-matrix-zeroes.java
@@ -2,9 +2,6 @@ class Solution {
     public void setZeroes(int[][] matrix) {
         boolean[] row = new boolean[matrix.length];
         boolean[] col = new boolean[matrix[0].length];
-        //Iterate over the matrix
-        //if encounter a 0 mark that row and col to true
-        //Iterate over the  matrix again and set all the marked places to 0
         
         for(int i=0; i<matrix.length; i++){
             for(int j=0; j<matrix[0].length; j++){
