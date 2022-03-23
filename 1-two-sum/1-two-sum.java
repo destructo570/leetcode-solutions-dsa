c7d7ebf1d4a5bc6eb6d063f444fb86e47d460d71
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-23 20:21:54 +0530

    Update 1-two-sum.java

--- 1-two-sum/1-two-sum.java
+++ 1-two-sum/1-two-sum.java
@@ -1,15 +1,43 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] result = new int[2];
-        
+        return optimal(nums, target);
+    }
+    
+    public int[] optimal(int[] nums, int target){
+        //Loop through the array and store it in a hashmap and value should be the index
+        //Now loop through the array again and check if (current-target) is in the hashmap
+        //if yes return the indices
+        HashMap<Integer, Integer> hs = new HashMap<>();
+        int[] res = new int[2];
         for(int i=0; i<nums.length; i++){
-            for(int j=0; j<nums.length; j++){
-                if(i!=j && nums[i]+nums[j] == target){
-                    result[0]=i;            
-                    result[1]=j;
+            hs.put(nums[i], i);
+        }
+        
+        for(int j=0; j<nums.length; j++){
+            if(hs.containsKey(target-nums[j]) && hs.get(target-nums[j]) != j){
+                res[0]=j;
+                res[1]=hs.get(target-nums[j]);
+                break;
+            }    
+        }
+        return res;
+    }
+    
+    public int[] naive(int[] nums, int target){
+        //Loop through the array and select one element
+        //Add this to all elements one by one and check if they equal to target
+        //if yes return the indices
+        int[] res = new int[2];
+        for(int i=0; i<nums.length; i++){
+            for(int j=i+1; j<nums.length; j++){
+                int sum=nums[i]+nums[j];
+                if(sum==target){
+                    res[0]=i;
+                    res[1]=j;
+                    return res;
                 }
             }
         }
-        return result;
+        return res;
     }
 }
