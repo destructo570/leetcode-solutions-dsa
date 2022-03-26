d3566ecae1a74cb1e65ec9319c4abf2e94e651d5
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-26 12:34:25 +0530

    Time: 4 ms (11.54%), Space: 48.3 MB (10.43%) - LeetHub

---
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        
+        //Store array elements in the set
+        //Loop through the arr and check if it is present in the set or not and cur is not euqal to this then add it to arr
+        
+        HashSet<Integer> hs = new HashSet<>();
+        for(int i=0; i<nums.length; i++)
+            hs.add(nums[i]);
+        
+        int cur = nums[0];
+        int ri = 1;
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=cur){
+                nums[ri++] = nums[i];
+                cur = nums[i];
+            }
+        }
+        
+        return hs.size();
+    }
+    
+    public int naive(int[] nums){
+        int count = 0;
+        
+        for(int i=0; i<nums.length; i++){
+           
+            for(int j=i+1; j<nums.length; j++){
+                
+                if(nums[i]==nums[j]){
+                    
+                    for(int k=j; k<nums.length; k++){
+                        if(nums[k]>nums[j]){
+                            nums[j]=nums[k];
+                            break;
+                        }
+                    }
+                }
+            }
+            if(nums[i]!=nums[nums.length-1]) count++;
+            
+        }
+        return count+1;
+    }
+}
