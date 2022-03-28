a10e71ba3800b48a223401e0dd594b3c1817050c
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-28 21:54:43 +0530

    Time: 1 ms (94.58%), Space: 50.1 MB (74.19%) - LeetHub

--- 136-single-number/136-single-number.java
+++ 136-single-number/136-single-number.java
@@ -1,5 +1,15 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int tmp=0;
+        
+        for(int i=0; i<nums.length; i++)
+            tmp^=nums[i];
+        
+        return tmp;
+    }   
+    
+    
+    public int singleNumberHashing(int[] nums) {
         HashMap<Integer, Integer> hm = new HashMap<>();
         
         for(int i=0; i<nums.length; i++)
@@ -9,5 +19,5 @@ class Solution {
             if(hm.get(nums[i]) == 1) return nums[i];
         
         return 0;
-    }   
+    } 
 }
