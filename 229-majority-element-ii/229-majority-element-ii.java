ce933342d62e977085a8811e4db59c2af7216d0a
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-30 23:15:28 +0530

    Time: 2 ms (92.35%), Space: 50 MB (44.48%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,5 +1,54 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
+        
+        return usingBoyerMoore(nums);
+            
+    }
+    
+    public List<Integer> usingBoyerMoore(int[] nums) {
+        
+        List<Integer> result = new ArrayList<>();
+        
+        if(nums.length == 0)
+            return result;
+        
+        int num1=nums[0];int num2=nums[0]; int count1=0; int count2=0;
+        
+        
+        for(int num: nums){
+            
+            if(num1==num) count1++;
+            else if(num2==num) count2++;
+            else if(count1==0){
+                num1=num;
+                count1++;
+            }else if(count2==0){
+                num2=num;
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        int c1=0; int c2=0;
+        
+        for(int num:nums){
+            if(num==num1){
+                c1++;
+            }else if(num==num2){
+                c2++;
+            }
+        }
+        
+        if(c1>nums.length/3) result.add(num1);
+        if(c2>nums.length/3) result.add(num2);
+        return result;
+            
+    }
+    
+    public List<Integer> usingHashing(int[] nums) {
         //Store the frequency in a hashmap
         //traverse and return all elements that appear >n/3 time in a array list
         
