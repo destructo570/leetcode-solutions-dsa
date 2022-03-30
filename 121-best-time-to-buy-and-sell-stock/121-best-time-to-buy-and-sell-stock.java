54f22f940045b3cb4c2ebed0dedabedcdd32c039
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-03-30 15:43:17 +0530

    Time: 3 ms (59.22%), Space: 84.9 MB (7.77%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,5 +1,22 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int profit=0;
+        int profitIfSoldToday=0;
+        int leastPriceSoFar=Integer.MAX_VALUE;
+
+
+        for(int i=0; i<prices.length; i++){
+            if(leastPriceSoFar>prices[i])
+                leastPriceSoFar=prices[i];
+            
+            profitIfSoldToday = prices[i] - leastPriceSoFar;
+            
+            if(profitIfSoldToday>profit) profit = profitIfSoldToday;
+        }
+        return profit;
+    }
+    
+    public int optimalOne(int[] prices){
         int maxCur=0; int maxSoFar=0;
         
         for(int i=1; i<prices.length; i++){
@@ -7,21 +24,6 @@ class Solution {
             maxSoFar = Math.max(maxCur, maxSoFar);
         }
         return maxSoFar;
-    }
-    
-    public int optimalOne(int[] prices){
-        int minIdx=0;
-        int profit=0;
-        for(int i=0; i<prices.length; i++){
-             if(prices[i]<prices[minIdx]) minIdx=i;
-        }
-        
-        for(int j=minIdx+1; j<prices.length; j++){
-            if(prices[j]-prices[minIdx] > profit){
-                profit = prices[j]-prices[minIdx];
-            }   
-        } 
-        return profit;
     }
     
     public int naive(int[] prices){
