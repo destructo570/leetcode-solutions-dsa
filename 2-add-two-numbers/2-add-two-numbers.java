1a106942276d5fc13db1d9269c3345f59a321dc6
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-09 16:55:50 +0530

    Time: 2 ms (82.72%), Space: 47.4 MB (73.35%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -0,0 +1,78 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        
+        //Find longer number and store it int num1
+        ListNode num1 = l1;
+        ListNode num2 = l2;
+        ListNode head = null;
+        
+        while(num1!=null || num2!=null){
+            if(num1==null){
+                num1 = l2;
+                num2 = l1;
+                break;
+            }else if(num2==null){
+                num1 = l1;
+                num2 = l2;
+                break;
+            }
+            num1=num1.next;
+            num2=num2.next;
+        }
+        
+        if(num1==null && num2==null){
+            num1=l1;
+            num2=l2;
+        }
+        
+        head = num1;
+        
+        int carry=0;
+        ListNode last = null;
+        while(num1!=null && num2!=null){
+            int sum = num1.val+num2.val+carry;
+            
+            if(sum>9){
+                carry=1;
+                num1.val=sum%10;
+            }else{
+                carry=0;
+                num1.val=sum;
+            }
+            if(num1.next==null) last=num1;
+            num1=num1.next;
+            num2=num2.next;
+        }
+        
+        while(num1!=null){
+            int sum = num1.val+carry;
+             if(sum>9){
+                carry=1;
+                num1.val=sum%10;
+            }else{
+                carry=0;
+                num1.val=sum;
+            }
+            if(num1.next==null) last=num1;
+            
+            num1=num1.next;
+            
+        }
+        
+        if(carry>0)
+           last.next = new ListNode(1);
+        
+        
+        return head;
+    }
+}
