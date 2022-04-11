9819ab5c35ea33df1bddb5da69ca57ffa67651ff
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-11 18:10:42 +0530

    Time: 150 ms (22.18%), Space: 57.2 MB (39.45%) - LeetHub

---
+++ 705-design-hashset/705-design-hashset.java
@@ -0,0 +1,57 @@
+class MyHashSet {
+
+    LinkedList<Integer>[] hash = new LinkedList[10];
+
+    public MyHashSet() {
+        
+    }
+    
+    public void add(int key) {
+        int index = getIndex(key);
+
+        if(hash[index]==null){
+            LinkedList<Integer> ll = new LinkedList<>();
+            ll.add(key);
+            hash[index] = ll;
+        }else{
+            if(getObject(key) == null) hash[index].add(key);
+        }
+    }
+
+    public int get(int key) {
+        Integer i = getObject(key);
+        return i==null?-1:i;
+    }
+
+    public void remove(int key) {
+        Integer i = getObject(key);
+        if(i!=null) hash[getIndex(key)].remove(i);
+    }
+    
+    public boolean contains(int key){
+        Integer i = getObject(key);
+        return i != null;
+    }
+
+    public Integer getObject(int key){
+        LinkedList<Integer> ll = hash[getIndex(key)];
+        
+        if(ll==null) return null;
+
+        for(int element: ll)
+            if(element == key) return element;
+        return null;
+    }
+
+    public int getIndex(int key){
+        return key%hash.length;
+    }
+}
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
