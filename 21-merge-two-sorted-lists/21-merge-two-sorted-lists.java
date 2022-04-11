588da0f5c298af6e7de650f6be24d41c68dbfc74
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-11 20:36:18 +0530

    Time: 1 ms (73.56%), Space: 43.4 MB (25.62%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.java
@@ -18,36 +18,19 @@ class Solution {
         
         while(list1!=null && list2!=null){
             if(list1.val<list2.val){
-                tail.next=new ListNode(list1.val);
-                tail=tail.next;
+                tail.next=list1;
+                tail=list1;
                 list1=list1.next;
             }else{
-                tail.next=new ListNode(list2.val);
-                tail=tail.next;
+                tail.next=list2;
+                tail=list2;
                 list2=list2.next;
             }
         }
                                                                    
         tail.next = list1==null?list2:list1;
-        tail = tail.next;
         return result.next;
     }
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
-    
     
      public ListNode optimalTwo(ListNode l1, ListNode l2){
         if(l1==null) return l2;
