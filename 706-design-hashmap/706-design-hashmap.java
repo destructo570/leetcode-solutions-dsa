a1eea085c45180c18dbbb45537243c8169c1095a
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-11 17:38:02 +0530

    Time: 133 ms (11.22%), Space: 61.6 MB (65.85%) - LeetHub

--- 706-design-hashmap/706-design-hashmap.java
+++ 706-design-hashmap/706-design-hashmap.java
@@ -2,6 +2,7 @@ class MyHashMap {
     
     LinkedList<Entry>[] hash = new LinkedList[10];
 
+
     public void put(int key, int value) {
         int index = getIndex(key);
 
@@ -10,40 +11,37 @@ class MyHashMap {
             ll.add(new Entry(key, value));
             hash[index] = ll;
         }else{
-            LinkedList<Entry> ll = hash[index];
-
-            for (Entry listElement : ll) {
-                if(listElement.key == key){
-                    listElement.val = value;
-                    return;
-                }
-            }
-
-            ll.add(new Entry(key, value));
+            Entry e = getEntry(key);
+            if(e != null)
+                e.val = value;
+            else hash[index].add(new Entry(key, value));
         }
     }
 
     public int get(int key) {
-        LinkedList<Entry> ll = hash[getIndex(key)];
-        
-        if(ll==null) return -1;
-        
-        for (Entry listElement : ll) {
-            if(listElement.key == key)
-                return listElement.val;
-        }
-        
-        return -1;
+        Entry e = getEntry(key);
+        return e==null?-1:e.val;
     }
 
     public void remove(int key) {
-        LinkedList<Entry> ll = hash[getIndex(key)];
-        if(ll==null) return;
-        ll.removeIf(listElement -> listElement.key == key);
+        Entry e = getEntry(key);
+        if(e!=null) hash[getIndex(key)].remove(e);
     }
 
     public int getIndex(int key){
         return key%hash.length;
+    }
+
+    public Entry getEntry(int key){
+        LinkedList<Entry> ll = hash[getIndex(key)];
+
+        if(ll != null) {
+            for (Entry listElement : ll) {
+                if (listElement.key == key)
+                    return listElement;
+            }
+        }
+        return null;
     }
 
     private class Entry{
