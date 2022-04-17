3be3ff6e4d11993bf0900b58b007464607b4d1ba
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-17 20:18:39 +0530

    Time: 1 ms (89.74%), Space: 40.5 MB (95.17%) - LeetHub

--- 28-implement-strstr/28-implement-strstr.java
+++ 28-implement-strstr/28-implement-strstr.java
@@ -1,6 +1,11 @@
 class Solution {
     public static int strStr(String haystack, String needle) {
 
+        return optimal1(haystack, needle);
+    }
+    
+        public static int naive(String haystack, String needle) {
+
         int ptr=0;
         if(haystack.length()<needle.length()) return -1;
 
@@ -11,6 +16,7 @@ class Solution {
                 for(int j=0; j<needle.length(); j++){
                     if(ptr>=haystack.length() || haystack.charAt(ptr) != needle.charAt(j)){
                         flag=false;
+                        
                         break;
                     }
                     ptr++;
@@ -21,5 +27,15 @@ class Solution {
 
         return -1;
     }
+    
+    public static int optimal1(String haystack, String needle) {
+      for (int i = 0; ; i++) {
+        for (int j = 0; ; j++) {
+          if (j == needle.length()) return i;
+          if (i + j == haystack.length()) return -1;
+          if (needle.charAt(j) != haystack.charAt(i + j)) break;
+    }
+  }
+}
 
 }
