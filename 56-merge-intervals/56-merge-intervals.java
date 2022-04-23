3e9e25ef4362a541351218d191818d623bf8add8
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-23 18:24:10 +0530

    Time: 7 ms (90.42%), Space: 47.3 MB (83.28%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,13 +1,9 @@
 class Solution {
     public static int[][] merge(int[][] arr) {
-        //if (arr.length <= 1) return arr;
-        
         Arrays.sort(arr, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-        int row=arr.length;
-        int col=arr[0].length;
 
         int j=0;
-        for(int k=0; k<row; k++){
+        for(int k=0; k<arr.length; k++){
             if(arr[j][1]>=arr[k][0])
                 arr[j] = new int[]{arr[j][0], Math.max(arr[j][1], arr[k][1])};
             else
