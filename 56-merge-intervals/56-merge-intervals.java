439579378e0d488254c6f27787fd9852a9f1268c
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-23 18:26:51 +0530

    Time: 8 ms (83.53%), Space: 47.4 MB (80.43%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,7 +1,6 @@
 class Solution {
     public static int[][] merge(int[][] arr) {
         Arrays.sort(arr, (i1, i2) -> Integer.compare(i1[0], i2[0]));
-
         int j=0;
         for(int k=0; k<arr.length; k++){
             if(arr[j][1]>=arr[k][0])
@@ -9,7 +8,6 @@ class Solution {
             else
                 arr[++j] = arr[k];
         }
-
         return Arrays.copyOfRange(arr, 0, j+1);
     }
 }
