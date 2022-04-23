9bb7f7378c63f4b9513871b5d8c83b983eed8ad7
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-23 16:54:53 +0530

    Time: 6 ms (99.22%), Space: 55 MB (64.63%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.java
@@ -0,0 +1,68 @@
+class Solution {
+    public static int[][] merge(int[][] intervals) {
+        
+        quickSort(intervals, 0, intervals.length-1);
+        int row=intervals.length;
+        int col=intervals[0].length;
+        
+        if(row==1) return intervals;
+
+        int j=0; int k=0;
+        while(k<row){
+            int[] a = intervals[j];
+            int[] b = intervals[k];
+            
+            if(a[0]>=b[0]){
+                int[] r = new int[]{b[0], Math.max(a[1], b[1])};
+                intervals[j] = r;
+            }else if(a[1]>=b[0]){
+                int[] r = new int[]{a[0], Math.max(a[1], b[1])};
+                intervals[j] = r;
+            }else{
+                intervals[++j] = intervals[k];
+            }
+            k++;
+
+        }
+
+        int[][] r2 = new int[j+1][col];
+        for(int i=0; i<=j; i++)
+            r2[i] = intervals[i];
+        return r2;
+    }
+    
+    
+
+    
+    public static void quickSort(int[][] arr, int l, int r){
+        if(l<r) {
+        int pi = partitionIndex(arr, l, r);
+        quickSort(arr, l, pi-1);
+        quickSort(arr, pi+1, r);
+        }
+    }
+
+    public static int partitionIndex(int[][] arr, int l, int r){
+        int[] pivot = arr[r];
+        int i=l;
+        int j=l;
+        while (i<=r){
+            if(arr[i][0]<pivot[0]){
+                swap(arr, i, j);
+                j++;
+            }if(arr[i][0]==pivot[0] && arr[i][1]<=pivot[1]){
+                swap(arr, i, j);
+                j++;
+            }
+            i++;
+        }
+
+        return j-1;
+    }
+
+    public static void swap(int[][] arr, int i, int j){
+        int[] tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+}
