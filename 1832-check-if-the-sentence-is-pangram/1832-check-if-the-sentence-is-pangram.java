183da31d729abe690b471458d5156b6143ca78eb
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-04-30 21:12:39 +0530

    Time: 3 ms (41.55%), Space: 42.4 MB (32.92%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.java
@@ -1,12 +1,8 @@
 class Solution {
     public boolean checkIfPangram(String s) {
         int[] arr = new int[26];
-        for(char c: s.toCharArray()) {
-            if ((int) c >= 65 && (int) c <= 90)
-                ++arr[c - 'A'];
-            if ((int) c >= 97 && (int) c <= 122)
+        for(char c: s.toCharArray()) 
                 ++arr[c - 'a'];
-        }
 
         for(int i=0; i<arr.length ; i++)
             if(arr[i] == 0) return false;
