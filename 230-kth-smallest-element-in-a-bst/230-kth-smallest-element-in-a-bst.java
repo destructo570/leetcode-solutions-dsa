9e77322adb46f2c5d62f2dd91a2d5f84fcaaba00
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-10 22:32:52 +0530

    Time: 5 ms (5.52%), Space: 46.8 MB (5.74%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -17,7 +17,22 @@
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
         List<Integer> result = new ArrayList<>();
-        inOrderTraversal(root, result);
+        
+        if(root==null) return -1;
+        
+        Stack<TreeNode> stack = new Stack<>();
+        
+        while(root!=null || !stack.isEmpty()){
+            while(root!=null){
+                stack.push(root);
+                root=root.left;
+            }
+            
+            root=stack.pop();
+            result.add(root.val);
+            root=root.right;
+        }
+        
         return result.get(k-1);
     }
     
@@ -28,4 +43,19 @@ class Solution {
         result.add(root.val);
         inOrderTraversal(root.right, result);
     }
+    
+// ============  Using Recursion
+//     public int kthSmallest(TreeNode root, int k) {
+//         List<Integer> result = new ArrayList<>();
+//         inOrderTraversal(root, result);
+//         return result.get(k-1);
+//     }
+    
+//     public void inOrderTraversal(TreeNode root, List<Integer> result) {
+//         if(root==null) return;
+        
+//         inOrderTraversal(root.left, result);
+//         result.add(root.val);
+//         inOrderTraversal(root.right, result);
+//     }
 }
