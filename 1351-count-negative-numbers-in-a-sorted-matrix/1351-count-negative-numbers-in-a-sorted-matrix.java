89a8e3f0631da4f8f38cb83cad4eb38d8991a803
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-12 09:25:36 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (94.16%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,27 +1,45 @@
 class Solution {
     public int countNegatives(int[][] grid) {
+        int row=grid.length; int col=grid[0].length; int m=row-1; int n=0;
         int count=0;
-        for(int i=0; i<grid.length; i++){
-            count+=count(grid[i]);
+        
+        while(m>=0 && n<col){
+            if(grid[m][n]<0){
+                m--;
+                count+=col-n;
+            }else{
+                n++;
+            }
         }
+        
         return count;
     }
+
     
-    public int count(int[] arr){
-        int left=0; int right=arr.length-1;
+    
+//     public int countNegatives(int[][] grid) {
+//         int count=0;
+//         for(int i=0; i<grid.length; i++){
+//             count+=count(grid[i]);
+//         }
+//         return count;
+//     }
+    
+//     public int count(int[] arr){
+//         int left=0; int right=arr.length-1;
         
-        while(left<=right){
-            int mid=(left+right)/2;
-            if(arr[mid]<0 && mid==0){
-                return arr.length;
-            }else if(arr[mid]<0 && arr[mid-1]>=0){
-                return arr.length-mid;
-            }else if(arr[mid]>=0){
-                left=mid+1;
-            }else{
-                right=mid-1;
-                }
-            }
-         return 0;
-    }
+//         while(left<=right){
+//             int mid=(left+right)/2;
+//             if(arr[mid]<0 && mid==0){
+//                 return arr.length;
+//             }else if(arr[mid]<0 && arr[mid-1]>=0){
+//                 return arr.length-mid;
+//             }else if(arr[mid]>=0){
+//                 left=mid+1;
+//             }else{
+//                 right=mid-1;
+//                 }
+//             }
+//          return 0;
+//     }
 }
