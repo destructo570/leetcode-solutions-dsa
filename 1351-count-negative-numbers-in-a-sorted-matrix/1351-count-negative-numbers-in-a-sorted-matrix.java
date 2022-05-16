fdbce14426ed337a2ec481954ebe637b56dcf160
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-16 11:41:26 +0530

    Time: 0 ms (100.00%), Space: 49 MB (61.33%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -1,5 +1,38 @@
 class Solution {
     public int countNegatives(int[][] grid) {
+        return binarySearchSol(grid);
+    }
+    
+    public int binarySearchSol(int[][] grid) {
+        int count=0;
+        
+        for(int[] arr:grid)
+            count+=binarySearch(arr);
+        
+        return count;
+    }
+    
+    public int binarySearch(int[] arr) {
+        int left=0; int right=arr.length-1;
+        if(arr.length==0) return 0;
+        if(arr.length==1) return arr[0]<0?1:0;
+        
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(mid==0){
+                if(arr[mid]<0)
+                    return arr.length;
+                else left = mid+1;
+            }else if(arr[mid]<0 && arr[mid-1]>=0)
+                return arr.length-mid;
+            else if(arr[mid-1]<0) right=mid-1;
+            else left=mid+1;
+        }
+        
+        return 0;
+    }
+    
+    public int optimal(int[][] grid) {
 
         int row=grid.length; int col=grid[0].length;
         int m=row-1; int n=0;
