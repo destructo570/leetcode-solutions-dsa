150559cfb84bd71129808947839dc60e8551f7eb
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-16 13:17:37 +0530

    Time: 0 ms (100.00%), Space: 47.6 MB (15.89%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,43 +1,39 @@
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
+    public int[] searchRange(int[] arr, int target) {
         
         int[] result = {-1,-1};
         
-        int left=0; int right=nums.length-1;
+        int left=0; int right=arr.length-1;
+        
+        
         while(left<=right){
-            int mid = (left+right)/2;
+            int mid=(left+right)/2;
             
-            if(nums[mid]==target){
-                if(mid==0 || nums[mid-1]!=target){
-                    result[0]=mid;
-                    break;
-                }else{
-                    right=mid-1;
-                }
-            }else if(nums[mid]<target){
+            if(mid==0 && arr[mid]==target){
+                result[0]=mid;
+                break;
+            }else if(arr[mid]==target && arr[mid-1]!=target){
+                result[0]=mid;
+                break;
+            }else if(arr[mid]<target)
                 left=mid+1;
-            }else{
-                right=mid-1;
-            }
+            else right=mid-1;
         }
         
-        left=0; right=nums.length-1;
+        left=0; right=arr.length-1;
         
         while(left<=right){
-            int mid = (left+right)/2;
+            int mid=(left+right)/2;
             
-            if(nums[mid]==target){
-                if(mid==nums.length-1 || nums[mid+1]!=target){
-                    result[1]=mid;
-                    break;
-                }else{
-                    left=mid+1;
-                }
-            }else if(nums[mid]<target){
+            if(mid==arr.length-1 && arr[mid]==target){
+                result[1]=mid;
+                break;
+            }else if(arr[mid]==target && arr[mid+1]!=target){
+                result[1]=mid;
+                break;
+            }else if(arr[mid]<=target)
                 left=mid+1;
-            }else{
-                right=mid-1;
-            }
+            else right=mid-1;
         }
         
         return result;
