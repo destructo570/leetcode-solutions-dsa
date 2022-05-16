f2c87ef6462e5eda3d11d2c6efd5448a832221d2
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-16 11:50:30 +0530

    Time: 1 ms (43.81%), Space: 49.5 MB (29.25%) - LeetHub

--- 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
+++ 1351-count-negative-numbers-in-a-sorted-matrix/1351-count-negative-numbers-in-a-sorted-matrix.java
@@ -18,14 +18,12 @@ class Solution {
         
         while(left<=right){
             int mid=(left+right)/2;
-            if(mid==0){
-                if(arr[mid]<0)
-                    return arr.length;
-                else left = mid+1;
-            }else if(arr[mid]<0 && arr[mid-1]>=0)
+            if(arr[mid]<0 && mid==0)
+                return arr.length;
+            else if(arr[mid]<0 && arr[mid-1]>=0)
                 return arr.length-mid;
-            else if(arr[mid-1]<0) right=mid-1;
-            else left=mid+1;
+            else if(arr[mid]>=0) left=mid+1;
+            else right=mid-1;
         }
         
         return 0;
