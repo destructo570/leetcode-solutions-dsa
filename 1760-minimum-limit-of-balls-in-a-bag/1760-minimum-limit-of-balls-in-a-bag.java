843e4aeb646176e775faf2b266fa5ca546edb463
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-17 16:13:56 +0530

    Time: 42 ms (83.90%), Space: 54.4 MB (69.35%) - LeetHub

--- 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
+++ 1760-minimum-limit-of-balls-in-a-bag/1760-minimum-limit-of-balls-in-a-bag.java
@@ -1,12 +1,13 @@
 class Solution {
     public int minimumSize(int[] nums, int maxOperations) {
-        int left=1, right=max(nums);
+        int left=1, right=1_000_000_000;
         
         while(left<right){
             int mid=left+(right-left)/2;
-            if(feasible(nums, maxOperations,mid)){
+            if(feasible(nums, maxOperations,mid))
                 right=mid;
-            }else left=mid+1;
+            else 
+                left=mid+1;
         }
         
         return left;
@@ -14,25 +15,8 @@ class Solution {
     
     public boolean feasible(int[] nums, int maxOperations, int k) {
         int count=0;
-        
-        for(int num:nums){
+        for(int num:nums)
             count+=(num - 1) / k;
-        }
-        
         return count<=maxOperations;
-    }
-    
-    public int max(int[] arr){
-        int max=Integer.MIN_VALUE;
-        for(int num:arr)
-            max=Math.max(max,num);
-        return max;
-    }
-    
-    public int min(int[] arr){
-        int min=Integer.MAX_VALUE;
-        for(int num:arr)
-            min=Math.min(min,num);
-        return min;
     }
 }
