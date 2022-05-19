f1447d9a24d45ed768e6f54dd11d57f1d1fe516a
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-19 16:50:53 +0530

    Time: 8 ms (14.71%), Space: 43.5 MB (32.24%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -14,17 +14,39 @@
  * }
  */
 class Solution {
-    int max=0;
+    //int max=0;
     public int diameterOfBinaryTree(TreeNode root) {
-        depth(root);
+        Stack<TreeNode> stack = new Stack<>();
+        int max = 0;
+        HashMap<TreeNode, Integer> hm = new HashMap<>();
+        stack.push(root);
+        while(!stack.isEmpty()){
+            TreeNode top = stack.peek();
+            
+            if(top.left!=null && !hm.containsKey(top.left))
+                stack.push(top.left);
+            else if(top.right!=null && !hm.containsKey(top.right))
+                stack.push(top.right);
+            else{
+                TreeNode node = stack.pop();
+                hm.put(node, Math.max(hm.getOrDefault(node.left,0),hm.getOrDefault(node.right,0))+1);
+                max = Math.max(max, hm.getOrDefault(node.left,0)+hm.getOrDefault(node.right,0));
+            }
+        }
         return max;
     }
+
     
-    public int depth(TreeNode root) {
-        if(root==null) return 0;
-        int left = depth(root.left);
-        int right = depth(root.right);
-        max=Math.max(max, left+right);
-        return Math.max(left,right)+1;
-    }
+//     public int recursiveSolution(TreeNode root) {
+//         depth(root);
+//         return max;
+//     }
+    
+//     public int depth(TreeNode root) {
+//         if(root==null) return 0;
+//         int left = depth(root.left);
+//         int right = depth(root.right);
+//         max=Math.max(max, left+right);
+//         return Math.max(left,right)+1;
+//     }
 }
