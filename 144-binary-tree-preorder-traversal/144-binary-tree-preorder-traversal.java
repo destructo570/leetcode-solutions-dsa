d0e5be8f3f167a596f7c8d43de7054e5edac3a8f
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-19 17:25:20 +0530

    Time: 1 ms (43.10%), Space: 40.7 MB (77.39%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.java
@@ -19,14 +19,40 @@ class Solution {
         if(root==null) return result;
         Stack<TreeNode> st = new Stack<>();
         st.push(root);
+        result.add(root.val);
+        TreeNode lastPop = root;
         while(!st.isEmpty()){
-            TreeNode current = st.pop();
-            result.add(current.val);
-            if(current.right!=null) st.push(current.right);
-            if(current.left!=null) st.push(current.left);
+            TreeNode top = st.peek();
+            if(top.left!=null && top.left!=lastPop && top.right!=lastPop){
+                st.push(top.left);
+                result.add(top.left.val);
+            }else if(top.right!=null && top.right!=lastPop && (top.left==null || top.left==lastPop)){
+                st.push(top.right);
+                result.add(top.right.val);
+            }else{
+                st.pop();
+                lastPop=top;
+            }
+            
         }
         
         
         return result;
     }
+    
+//     public List<Integer> preorderTraversalOne(TreeNode root) {
+//         ArrayList<Integer> result = new ArrayList<>();
+//         if(root==null) return result;
+//         Stack<TreeNode> st = new Stack<>();
+//         st.push(root);
+//         while(!st.isEmpty()){
+//             TreeNode current = st.pop();
+//             result.add(current.val);
+//             if(current.right!=null) st.push(current.right);
+//             if(current.left!=null) st.push(current.left);
+//         }
+        
+        
+//         return result;
+//     }
 }
