a38f35368dce7309c2e52d46b9c7493fe47ee481
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-19 19:56:33 +0530

    Time: 10 ms (5.62%), Space: 45.7 MB (19.26%) - LeetHub

--- 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
+++ 230-kth-smallest-element-in-a-bst/230-kth-smallest-element-in-a-bst.java
@@ -13,42 +13,27 @@
  *     }
  * }
  */
-
 class Solution {
     public int kthSmallest(TreeNode root, int k) {
-        List<Integer> result = new ArrayList<>();
-        
         if(root==null) return -1;
-        
-        Stack<TreeNode> stack = new Stack<>();
-        
-        while(root!=null || !stack.isEmpty()){
-            while(root!=null){
-                stack.push(root);
-                root=root.left;
+        Stack<TreeNode> st = new Stack<>();
+        ArrayList<Integer> result = new ArrayList<>();
+        TreeNode lastPop = root;
+        st.push(root);
+        while(!st.isEmpty()){
+            TreeNode top = st.peek();
+            if(top.left!=null && top.left!=lastPop && top.right!=lastPop){
+                st.push(top.left);
+            }else if(top.right!=null && top.right!=lastPop){
+                st.push(top.right);
+                result.add(top.val);
+            }else{
+                st.pop();
+                lastPop=top;
+                if(top.right==null) result.add(top.val);
             }
-            
-            root=stack.pop();
-            result.add(root.val);
-            root=root.right;
         }
         
         return result.get(k-1);
     }
-    
-    
-// ============  Using Recursion
-//     public int kthSmallest(TreeNode root, int k) {
-//         List<Integer> result = new ArrayList<>();
-//         inOrderTraversal(root, result);
-//         return result.get(k-1);
-//     }
-    
-//     public void inOrderTraversal(TreeNode root, List<Integer> result) {
-//         if(root==null) return;
-        
-//         inOrderTraversal(root.left, result);
-//         result.add(root.val);
-//         inOrderTraversal(root.right, result);
-//     }
 }
