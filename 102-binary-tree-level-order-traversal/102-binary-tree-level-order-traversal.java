34bafed9ca7eac0f62deb9215fad2490a8dfa5e9
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-19 11:43:55 +0530

    Time: 1 ms (83.91%), Space: 44.1 MB (12.14%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,47 +14,28 @@
  * }
  */
 class Solution {
-        public List<List<Integer>> levelOrder(TreeNode root) {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        Queue<TreeNode> q = new LinkedList<>();
         List<List<Integer>> result = new ArrayList<>();
-        traverse(root, result, 0);
+        if (root == null) return result; 
+        q.offer(root);
+        while(!q.isEmpty()){
+            List<Integer> res = new ArrayList<>();
+            int level = q.size();
+            
+            for(int i=0; i<level; i++){
+                TreeNode n = q.remove();
+                res.add(n.val);
+                if(n.left != null) q.add(n.left);
+            
+                if(n.right != null) q.add(n.right);
+                
+            }
+
+            result.add(res);
+        }
+        
         return result;
     }
     
-    public void traverse(TreeNode root, List<List<Integer>> result, int level){
-        if(root==null) return;
-        
-        if(result.size()==level)
-            result.add(new ArrayList<Integer>());
-        
-        result.get(level).add(root.val);
-        traverse(root.left, result, level+1);
-        traverse(root.right, result, level+1);
-    }
-    
-
-//     public List<List<Integer>> levelOrder(TreeNode root) {
-//         List<List<Integer>> result = new ArrayList<>();
-//         for(int i=0; i<=height(root); i++){
-//             List<Integer> tmp = new ArrayList<>();
-//             getNodesAtKDistance(root,i,tmp);
-//             result.add(tmp);
-//         }
-//         return result;
-//     }
-    
-//     public int height(TreeNode root){
-//         if(root==null) return -1;  
-//         if(root.left==null && root.right==null) return 0;
-//         return 1+Math.max(height(root.left), height(root.right));
-//     }
-
-//     private void getNodesAtKDistance(TreeNode root, int distance, List<Integer> result){
-//         if(root==null) return;
-//         if(distance==0){
-//             result.add(root.val);
-//             return;
-//         }
-//         getNodesAtKDistance(root.left, distance-1, result);
-//         getNodesAtKDistance(root.right, distance-1, result);
-//     }
 }
