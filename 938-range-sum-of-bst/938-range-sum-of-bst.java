d429d5f265acb308add7331ec49f411a370d7609
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-20 10:37:10 +0530

    Time: 34 ms (5.26%), Space: 64.4 MB (69.96%) - LeetHub

--- 938-range-sum-of-bst/938-range-sum-of-bst.java
+++ 938-range-sum-of-bst/938-range-sum-of-bst.java
@@ -14,7 +14,34 @@
  * }
  */
 class Solution {
+    
     public int rangeSumBST(TreeNode root, int low, int high) {
+        int sum=0;
+        Stack<TreeNode> st = new Stack<TreeNode>();
+        while(!st.isEmpty() || root!=null){
+            while(root!=null){
+                st.push(root);
+                root=root.left;
+            }
+            
+            root=st.pop();
+            if((root.val >= low && root.val<=high)) sum+=root.val;
+            root=root.right;
+        }
+        
+        return sum;
+    }
+    
+    
+    public int traverse(TreeNode root, int low, int high, int sum) {
+        if(root==null) return 0;
+        
+        if((root.val >= low && root.val<=high)) return sum+=root.val;
+        
+        
+        return traverse(root.left, low, high, sum)+traverse(root.right, low, high, sum);
+    }
+    public int iterative(TreeNode root, int low, int high) {
         int sum=0;
         Stack<TreeNode> st = new Stack<TreeNode>();
         while(!st.isEmpty() || root!=null){
