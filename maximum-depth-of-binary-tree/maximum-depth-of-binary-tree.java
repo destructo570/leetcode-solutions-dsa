2838b56f1006c16520753f93d3d396a432a93357
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-22 21:22:52 +0530

    Time: 0 ms, Memory: 41.7 MB - LeetHub

--- maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
+++ maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.java
@@ -16,6 +16,13 @@
 class Solution {
      int max=0;
     public int maxDepth(TreeNode root) {
+        if(root==null) return 0;
+        int left=maxDepth(root.left)+1;
+        int right=maxDepth(root.right)+1;
+        return Math.max(left,right);
+    }
+    
+    public int topDown(TreeNode root) {
         depth(root,1);
         return max;
     }
