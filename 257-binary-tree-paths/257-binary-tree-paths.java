f63887008987bc092c20a4038875b8d35caed2ba
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-23 11:54:36 +0530

    Time: 14 ms (21.45%), Space: 43.6 MB (20.65%) - LeetHub

--- 257-binary-tree-paths/257-binary-tree-paths.java
+++ 257-binary-tree-paths/257-binary-tree-paths.java
@@ -16,16 +16,15 @@
 class Solution {
     public List<String> binaryTreePaths(TreeNode root) {
         List<String> result = new ArrayList<>();
-        recursive(root,result,"");
+        recursiveNaive(root,result,"");
         return result;
     }
     
-    public void recursive(TreeNode root, List<String> ans,String path) {
+    public void recursiveNaive(TreeNode root, List<String> ans,String path) {
         if(root==null) return;
-        path = path+root.val;
+        path += (path.length() == 0 ? "" : "->") +root.val;
         if(root.left==null && root.right==null) ans.add(path);
-        else path=path+"->";
-        recursive(root.left,ans,path);
-        recursive(root.right,ans,path);
+        recursiveNaive(root.left,ans,path);
+        recursiveNaive(root.right,ans,path);
     }
 }
