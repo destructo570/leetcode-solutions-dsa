3cfb416c9c7cb00e7f7c82df86b623386c1623c0
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-24 14:21:15 +0530

    Time: 27 ms (7.73%), Space: 44.2 MB (65.28%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.java
@@ -6,21 +6,28 @@ class Solution {
     }
     
     
-    public int quickSelect(int[] nums, int lo, int hi, int k) {
-        
-       int j = lo;
-       
-        for(int i=lo; i<hi; i++){
-            if(nums[i]<=nums[hi])
-                swap(nums, i, j++);
-        }
-        swap(nums,j,hi);
-        int count=hi-j+1;
-        if(count==k) return nums[j];
-        if(count>k) return quickSelect(nums,j+1,hi,k);
-        
-        return quickSelect(nums,lo,j-1,k-count);
+int quickSelect(int[] nums, int low, int high, int k) {
+  int pivot = low;
+
+  // use quick sort's idea
+  // put nums that are <= pivot to the left
+  // put nums that are  > pivot to the right
+  for (int j = low; j < high; j++) {
+    if (nums[j] <= nums[high]) {
+      swap(nums, pivot++, j);
     }
+  }
+  swap(nums, pivot, high);
+  
+  // count the nums that are > pivot from high
+  int count = high - pivot + 1;
+  // pivot is the one!
+  if (count == k) return nums[pivot];
+  // pivot is too small, so it must be on the right
+  if (count > k) return quickSelect(nums, pivot + 1, high, k);
+  // pivot is too big, so it must be on the left
+  return quickSelect(nums, low, pivot - 1, k - count);
+}
     
     public void swap(int[] nums, int first, int second){
         int temp = nums[first];
