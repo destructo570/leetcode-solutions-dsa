6d77caefcb12c63f506ca1163ab190a37bafcd4f
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 11:31:59 +0530

    Time: 2 ms (68.66%), Space: 54.8 MB (47.61%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.java
+++ 283-move-zeroes/283-move-zeroes.java
@@ -1,17 +1,15 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-        
-        //Use two pointers i and j
-        //traverse the array and if at ith index value is > 0 swap with j and increment j
         int j=0;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]!=0){
-                int tmp = nums[i];
-                nums[i]=nums[j];
-                nums[j]=tmp;
-                j++;
-            }
-        }
         
+        for(int i=0; i<nums.length; i++){
+            if(nums[i]!=0) swap(nums, i, j++);
+        }
+    }
+    
+    public void swap(int[] arr, int i, int j){
+        int temp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=temp;
     }
 }
