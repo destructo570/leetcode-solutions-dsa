f7becb3284c010f76c8e1a4d05879b90dc9dc6f5
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 10:41:50 +0530

    Time: 2 ms (86.27%), Space: 77.6 MB (56.81%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.java
@@ -1,40 +1,17 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int profit=0;
-        int profitIfSoldToday=0;
-        int leastPriceSoFar=Integer.MAX_VALUE;
-
-
-        for(int i=0; i<prices.length; i++){
-            if(leastPriceSoFar>prices[i])
-                leastPriceSoFar=prices[i];
-            
-            profitIfSoldToday = prices[i] - leastPriceSoFar;
-            
-            if(profitIfSoldToday>profit) profit = profitIfSoldToday;
-        }
-        return profit;
-    }
-    
-    public int optimalOne(int[] prices){
-        int maxCur=0; int maxSoFar=0;
+        //get max profit 
+        //choosing lowest buy and max sell
+        //keeping track of max profit so far
+        int lowest = prices[0];
+        int maxProfit = Integer.MIN_VALUE;
         
         for(int i=1; i<prices.length; i++){
-            maxCur = Math.max(0, maxCur + prices[i]-prices[i-1]);
-            maxSoFar = Math.max(maxCur, maxSoFar);
+            int profit = prices[i]-lowest;
+            if(profit>maxProfit) maxProfit=profit;
+            if(prices[i]<lowest) lowest=prices[i];
         }
-        return maxSoFar;
-    }
-    
-    public int naive(int[] prices){
-        int profit=0;
-        for(int i=0; i<prices.length; i++){
-            for(int j=i+1; j<prices.length; j++){
-                if(prices[j]-prices[i] > profit){
-                    profit = prices[j]-prices[i];
-                }   
-            }   
-        }
-        return profit;
+        
+        return maxProfit < 0 ? 0 : maxProfit;
     }
 }
