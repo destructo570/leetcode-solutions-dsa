fa2e8b5d5b491f1d2356917ed0a591ba91d80271
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 11:43:39 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (70.85%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.java
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -7,7 +7,7 @@ class Solution {
         if(n==1) return 1;
         if(n==0) return 0;
         int a=0; int b=1;
-        for(int i=2; i<n+1; i++){
+        for(int i=2; i<=n; i++){
             int f = a+b;
             a=b;
             b=f;
