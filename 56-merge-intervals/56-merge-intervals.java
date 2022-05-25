3bf25237d6b6f03e8d6c3b3c8e88b324e2661f70
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 18:57:43 +0530

    Time: 14 ms (40.87%), Space: 54.9 MB (64.91%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,13 +1,13 @@
 class Solution {
-    public static int[][] merge(int[][] arr) {
-        Arrays.sort(arr, (i1, i2) -> Integer.compare(i1[0], i2[0]));
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (i1,i2)->Integer.compare(i1[0], i2[0]));
         int j=0;
-        for(int k=0; k<arr.length; k++){
-            if(arr[j][1]>=arr[k][0])
-                arr[j] = new int[]{arr[j][0], Math.max(arr[j][1], arr[k][1])};
-            else
-                arr[++j] = arr[k];
+        for(int i=j+1; i<intervals.length; i++){
+            if(intervals[j][1]>=intervals[i][0]){
+                intervals[j]=new int[]{intervals[j][0], Math.max(intervals[j][1], intervals[i][1])};
+            }else intervals[++j] = intervals[i];
+
         }
-        return Arrays.copyOfRange(arr, 0, j+1);
+        return Arrays.copyOfRange(intervals, 0, j+1);
     }
 }
