cb60a0484c1aeb1cf130ff354999e0032cef0566
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 13:52:57 +0530

    Time: 1 ms (99.98%), Space: 46.1 MB (83.77%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,6 +1,40 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        return hashmap(nums);
+        return boydMoore(nums);
+    }
+    
+    public List<Integer> boydMoore(int[] nums) {
+        
+        int num1=nums[0], num2=nums[0], count1=0, count2=0;
+        List<Integer> result = new ArrayList<>();
+        for(int num: nums){
+            if(num1==num)
+                count1++;
+            else if(num2==num)
+                count2++;
+            else if(count1==0){
+                num1=num;
+                count1++;
+            }else if(count2==0){
+                num2=num;
+                count2++;
+            }else{
+                count1--;
+                count2--;
+            }
+        }
+        
+        
+        int c1=0,c2=0;
+        
+        for(int num:nums){
+            if(num==num1) c1++;
+            else if(num==num2) c2++;
+        }
+        
+        if(c1>nums.length/3)result.add(num1);
+        if(c2>nums.length/3)result.add(num2);
+        return result;
     }
     
     public List<Integer> hashmap(int[] nums) {
