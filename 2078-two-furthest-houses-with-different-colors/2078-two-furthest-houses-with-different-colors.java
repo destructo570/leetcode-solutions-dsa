791247ae529c11a8c1d0f838d001b03b6a371855
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 11:08:54 +0530

    Time: 1 ms (59.09%), Space: 42.6 MB (17.11%) - LeetHub

--- 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
+++ 2078-two-furthest-houses-with-different-colors/2078-two-furthest-houses-with-different-colors.java
@@ -5,14 +5,10 @@ class Solution {
     
     public int optimal(int[] colors) {
         int max=0;
-        int i=0;
-        for(int j=colors.length-1; j>=0; j--){
-            if(colors[i]!=colors[j]) max = Math.max(max, Math.abs(i-j));
-        }
-        
-        i=colors.length-1;
-        for(int j=0; j<colors.length; j++){
-            if(colors[i]!=colors[j]) max = Math.max(max, Math.abs(i-j));
+        int j=colors.length-1;
+        for(int i=0; i<colors.length; i++){
+            if(colors[i]!=colors[j]) max = Math.max(max, Math.abs(i-j)); 
+            if(colors[i-i]!=colors[j-i]) max = Math.max(max, Math.abs((i-i)-(j-i)));
         }
         return max;
     }
