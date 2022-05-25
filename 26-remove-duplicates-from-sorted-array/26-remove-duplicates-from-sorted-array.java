719ee86bb220489e3e8d3c96048cf9a589a76d9c
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 12:55:17 +0530

    Time: 1 ms (94.53%), Space: 48 MB (28.09%) - LeetHub

--- 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/26-remove-duplicates-from-sorted-array.java
@@ -1,53 +1,22 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
+        int ptr=1, num=nums[0];
         
-        int ri = 0;
-        for(int i=0; i<nums.length; i++)
-            if(nums[i]!=nums[ri])
-                nums[++ri] = nums[i];
-        
-        return ++ri;
-    }
-    
-    public int optimalOne(int[] nums) {
-        
-        HashSet<Integer> hs = new HashSet<>();
-        for(int i=0; i<nums.length; i++)
-            hs.add(nums[i]);
-        
-        int cur = nums[0];
-        int ri = 1;
-        for(int i=0; i<nums.length; i++){
-            if(nums[i]!=cur){
-                nums[ri++] = nums[i];
-                cur = nums[i];
-            }
-        }
-        
-        return hs.size();
-    }
-    
-    
-    public int naive(int[] nums){
-        int count = 0;
-        
-        for(int i=0; i<nums.length; i++){
-           
-            for(int j=i+1; j<nums.length; j++){
+        for(int i=ptr; i<nums.length; i++){
+            if(nums[i]!=num) {
+                num=nums[i];
+                swap(nums, ptr++, i);
                 
-                if(nums[i]==nums[j]){
-                    
-                    for(int k=j; k<nums.length; k++){
-                        if(nums[k]>nums[j]){
-                            nums[j]=nums[k];
-                            break;
-                        }
-                    }
-                }
             }
-            if(nums[i]!=nums[nums.length-1]) count++;
-            
         }
-        return count+1;
+        
+        return ptr;
     }
+    
+    private void swap(int[]arr, int first, int second){
+        int temp = arr[first];
+        arr[first]=arr[second];
+        arr[second]=temp;
+    }
+
 }
