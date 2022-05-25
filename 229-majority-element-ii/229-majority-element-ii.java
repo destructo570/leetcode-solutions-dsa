4d41ca9d4e062784a7e299ac5fa77fbb41a530de
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 13:23:58 +0530

    Time: 7 ms (44.62%), Space: 45.7 MB (91.45%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,71 +1,17 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        
-        return usingBoyerMoore(nums);
-            
+        return hashmap(nums);
     }
     
-    public List<Integer> usingBoyerMoore(int[] nums) {
-        
+    public List<Integer> hashmap(int[] nums) {
+        HashMap<Integer,Integer> hm = new HashMap<>(); 
         List<Integer> result = new ArrayList<>();
+        for(int num:nums)
+            hm.put(num, hm.getOrDefault(num,0)+1);
         
-        if(nums.length == 0)
-            return result;
-        
-        int num1=nums[0];int num2=nums[0]; int count1=0; int count2=0;
-        
-        
-        for(int num: nums){
-            
-            if(num1==num) 
-                count1++;
-            else if(num2==num) 
-                count2++;
-            else if(count1==0){
-                num1=num;
-                count1++;
-            }else if(count2==0){
-                num2=num;
-                count2++;
-            }
-            else{
-                count1--;
-                count2--;
-            }
-        }
-        
-        int c1=0; int c2=0;
-        
-        for(int num:nums){
-            if(num==num1){
-                c1++;
-            }else if(num==num2){
-                c2++;
-            }
-        }
-        
-        if(c1>nums.length/3) result.add(num1);
-        if(c2>nums.length/3) result.add(num2);
-        return result;
-            
-    }
-    
-    public List<Integer> usingHashing(int[] nums) {
-        //Store the frequency in a hashmap
-        //traverse and return all elements that appear >n/3 time in a array list
-        
-        HashMap<Integer, Integer> hm = new HashMap<>();
-        List<Integer> result = new ArrayList<>();
-
-        for(int i=0; i<nums.length; i++)
-            hm.put(nums[i], hm.getOrDefault(nums[i], 0)+1);
-
-        for(Map.Entry<Integer,Integer> entry: hm.entrySet()){
-           if(entry.getValue() > nums.length/3)
-               result.add(entry.getKey());
-        }
+        for(Map.Entry<Integer, Integer> entry:hm.entrySet())
+            if(entry.getValue()>nums.length/3) result.add(entry.getKey());
         
         return result;
-            
     }
 }
