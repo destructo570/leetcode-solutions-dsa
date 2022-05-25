001bb0e957fa0b31b3157fe13b6b64e63d2e0b94
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-25 14:11:34 +0530

    Time: 2 ms (62.63%), Space: 54.5 MB (5.26%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -1,47 +1,13 @@
 class Solution {
     public int pivotIndex(int[] nums) {
-        
-        return optimalOne(nums);
-    }
-    
-    public int optimalOne(int[] nums) {
-        
-        int[] prefixSum = new int[nums.length+1];
-
-        for(int i=1; i<=nums.length; i++)
-            prefixSum[i] = nums[i-1] + prefixSum[i-1];
-
-        for(int i=0; i<nums.length; i++)
-            if(prefixSum[i]==(prefixSum[nums.length]-prefixSum[i+1])) return i;
-        
-        return -1;
-    }
-    
-    
-    public int bruteForce(int[] nums) {
-        
-        //Loop through the array and select an index
-        //calculate the sum of all elements to the left of the index and store it in leftSum
-        //calculate the sum of all elements to the right of the index and store it in rightSum
-        //if leftSum == rightSum return the index
-        
+        for(int i=1; i<nums.length; i++)
+            nums[i]=nums[i]+nums[i-1];
         
         for(int i=0; i<nums.length; i++){
-            int leftSum=0;
-            int rightSum=0;    
-            
-            for(int j=i-1; j>=0; j--)
-                leftSum+=nums[j];
-            
-            
-            for(int k=i+1; k<nums.length; k++)
-                rightSum+=nums[k];
-            
-            
-            if(leftSum==rightSum) return i;
-            
+            if(i==0 && (nums[nums.length-1]-nums[i])==0) return i;
+            if(i==nums.length-1 && nums[nums.length-2]==0) return i;
+            if(i-1>=0 && nums[i-1]==(nums[nums.length-1] - nums[i])) return i;
         }
-        
         return -1;
     }
 }
