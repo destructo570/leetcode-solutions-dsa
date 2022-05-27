fbf9ba30fd2e76ce6d34926f9e13868a6bc467e6
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-27 20:32:09 +0530

    Time: 28 ms (68.37%), Space: 58.7 MB (56.11%) - LeetHub

--- 15-3sum/15-3sum.java
+++ 15-3sum/15-3sum.java
@@ -1,6 +1,30 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        return twoPointer(nums);
+        return optimal(nums);
+    }
+    
+    public List<List<Integer>> optimal(int[] nums) {
+        
+        Arrays.sort(nums);
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=0; i<nums.length; i++){
+            if(i==0 || i>0 && nums[i]!=nums[i-1]){
+                int j=i+1, k=nums.length-1, sum=0-nums[i];
+            
+                while(j<k){
+                    if(nums[j]+nums[k]==sum){
+                        result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                        while(j<k && nums[j] == nums[j+1]) j++;
+                        while(j<k && nums[k] == nums[k-1]) k--;
+                        j++; k--;
+                    }else if(nums[j]+nums[k]>sum) k--;
+                     else j++;
+                }
+            }
+            
+        }
+        
+        return result;
     }
     
     public List<List<Integer>> twoPointer(int[] nums) {
