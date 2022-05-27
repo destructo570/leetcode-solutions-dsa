6fe8483584f27d66adba0abe223dccdc93e4ce34
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-27 11:04:25 +0530

    Time: 1 ms (99.63%), Space: 43.1 MB (86.57%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.java
+++ 724-find-pivot-index/724-find-pivot-index.java
@@ -9,7 +9,6 @@ class Solution {
         
         for(int i=0; i<nums.length; i++){
             if(i==0 && (nums[nums.length-1]-nums[i])==0) return i;
-            //if(i==nums.length-1 && nums[nums.length-2]==0) return i;
             if(i-1>=0 && nums[i-1]==(nums[nums.length-1] - nums[i])) return i;
         }
         return -1;
