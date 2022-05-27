89fd9bbae2b32beaa23af4336f78b548b34deaad
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-05-27 13:56:24 +0530

    Time: 3 ms (41.40%), Space: 53.7 MB (66.74%) - LeetHub

--- 495-teemo-attacking/495-teemo-attacking.java
+++ 495-teemo-attacking/495-teemo-attacking.java
@@ -2,8 +2,7 @@ class Solution {
     public int findPoisonedDuration(int[] timeSeries, int duration) {
         int prev=-1;
         int result=0;
-        int n = timeSeries.length;
-        for(int i=0; i<n; i++){
+        for(int i=0; i<timeSeries.length; i++){
             if(prev>=timeSeries[i])
                 result+=duration-(prev - timeSeries[i] + 1);
             else 
