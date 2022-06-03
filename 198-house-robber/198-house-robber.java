5b88c5d35ee58c62b09b1a2e72b00ee1a2b82802
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-06-03 12:47:18 +0530

    Time: 0 ms (100.00%), Space: 41.1 MB (66.56%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,32 +1,28 @@
 class Solution {
+    
     public int rob(int[] nums) {
-         int[] memo = new int[nums.length + 1];
-         Arrays.fill(memo, -1);
-        return helper(nums,nums.length-1,memo);
+    if (nums.length == 0) return 0;
+    int prev1 = 0;
+    int prev2 = 0;
+    for (int num : nums) {
+        int tmp = prev1;
+        prev1 = Math.max(prev2 + num, prev1);
+        prev2 = tmp;
     }
-    
-    public int helper(int[] nums, int i, int[] memo) {
-        if(i<0) return 0;
-        if(memo[i]>=0) return memo[i];
-        int result = Math.max(helper(nums,i-2,memo)+nums[i], helper(nums, i-1,memo));
-        memo[i] = result;
-        return result;
-    }
-    
-//     public int helper(int[] nums, int house) {
-//         if(house>nums.length-1) return 0;
-        
-//         int max = nums[house];
-        
-//         for(int i=house+2; i<nums.length; i++){
-            
-//             int result=helper(nums,i);
-            
-//             if(result>=0 && result>max){
-//                 max+=nums[i];
-//             }
-//         }
-        
-//         return max;
+    return prev1;
+}
+//     public int rob(int[] nums) {
+//          int[] memo = new int[nums.length + 1];
+//          Arrays.fill(memo, -1);
+//         return helper(nums,nums.length-1,memo);
 //     }
+    
+//     public int helper(int[] nums, int i, int[] memo) {
+//         if(i<0) return 0;
+//         if(memo[i]>=0) return memo[i];
+//         int result = Math.max(helper(nums,i-2,memo)+nums[i], helper(nums, i-1,memo));
+//         memo[i] = result;
+//         return result;
+//     }
+
 }
