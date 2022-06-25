25a35fc73e8eecc99dc38f607cd4e04f9a5e5587
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-06-25 17:33:28 +0530

    Time: 56 ms (96.95%), Space: 41.6 MB (87.38%) - LeetHub

---
+++ 70-climbing-stairs/70-climbing-stairs.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function(n) {
+    const dp = [];
+    return helper(n, dp);
+};
+
+function helper(n, dp){
+    if(n===0) return 1;
+    if(n<0) return 0;
+    if (dp[n] > 0) return dp[n];
+    let result = helper(n-1, dp) + helper(n-2, dp);
+    dp[n]=result;
+    return result;
+}
