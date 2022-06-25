4f4469cfa1b3ed4a93a9b953dc16de7a15bcaf48
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-06-25 16:58:53 +0530

    Time: 137 ms (24.91%), Space: 50.9 MB (18.78%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.js
+++ 53-maximum-subarray/53-maximum-subarray.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var maxSubArray = function(nums) {
-    return kaden(nums);
-};
-
-function kaden(nums){
-    let currMax = -Infinity;
+        let currMax = -Infinity;
     let max = -Infinity;
     for(let num of nums){
         currMax = Math.max(num,num+currMax);
         max = Math.max(currMax, max);
     }
     return max;
+};
+
+function kaden(nums){
+
 }
