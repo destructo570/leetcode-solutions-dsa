85253592c5c97e2a44aaa6a154c81dd1eafeb007
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-07-01 23:58:17 +0530

    Time: 2 ms (55.78%), Space: 49.8 MB (8.08%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.java
+++ 42-trapping-rain-water/42-trapping-rain-water.java
@@ -6,29 +6,16 @@ class Solution {
         int[] rightMax = new int[n];
         leftMax[0]=height[0];
         rightMax[n-1]=height[n-1];
-        for(int j=n-2; j>=0;j--){
+        for(int j=n-2; j>=0;j--)
             rightMax[j]=Math.max(rightMax[j+1], height[j]);
-        }
+        
             
-        for(int k=1; k<n;k++){
+        for(int k=1; k<n;k++)
             leftMax[k]=Math.max(leftMax[k-1], height[k]);
-        }
         
         for(int i=0; i<height.length;i++){
-//             int leftMax = 0;    
-//             int rightMax = 0;    
-            
-//             for(int j=i-1; j>=0;j--){
-//                 leftMax=Math.max(leftMax, height[j]);
-//             }
-            
-//             for(int k=i+1; k<height.length;k++){
-//                 rightMax=Math.max(rightMax, height[k]);
-//             }
-            
-            
             int calc = Math.min(leftMax[i], rightMax[i])-height[i];
-            count+= calc >=0 ? calc : 0;
+            count+= Math.min(leftMax[i], rightMax[i])-height[i];
         }
         return count;
     }
