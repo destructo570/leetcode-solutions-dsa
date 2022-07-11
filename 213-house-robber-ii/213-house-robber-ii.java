2327b132db382dc63032c2b29fd849d1ced21f3b
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-07-11 16:06:59 +0530

    Time: 0 ms (100.00%), Space: 41.6 MB (48.01%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,9 +1,9 @@
 class Solution {
     public int rob(int[] nums) {
         if(nums.length==1) return nums[0];
-        int[] dp = new int[nums.length+1];
+        int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
-        int[] dp1 = new int[nums.length+1];
+        int[] dp1 = new int[nums.length];
         Arrays.fill(dp1, -1);
         int ans1 = helper(nums, 0, dp, false);
         int ans2 = helper(nums, 1, dp1, true);
