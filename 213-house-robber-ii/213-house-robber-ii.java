5b30a2c7d6b748004c62722f43be61461429569f
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-07-11 16:15:47 +0530

    Time: 0 ms (100.00%), Space: 41.5 MB (48.01%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -1,5 +1,25 @@
-class Solution {
+class Solution {    
     public int rob(int[] nums) {
+        if(nums==null||nums.length==0) return 0;        
+        int exclude_1st = rob_helper(nums, 1, nums.length-1);
+        int include_1st = nums[0] + rob_helper(nums, 2, nums.length-2);
+        return Math.max(include_1st, exclude_1st);        
+    }
+
+    public int rob_helper(int[] nums, int start, int end) {     
+        int pre_i_2 = 0;
+        int pre_i_1 = 0;
+        for(int i=start; i<=end; i++)
+        {
+            int cur = Math.max(nums[i] + pre_i_2, pre_i_1);
+            int temp = pre_i_1;
+            pre_i_1 = cur;
+            pre_i_2 = temp;
+        }        
+        return pre_i_1;
+    }
+    
+    public int recursive(int[] nums) {
         if(nums.length==1) return nums[0];
         int[] dp = new int[nums.length];
         Arrays.fill(dp, -1);
