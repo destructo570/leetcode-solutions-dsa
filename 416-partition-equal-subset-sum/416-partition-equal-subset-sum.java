4ebdd55115cb5f17e6fc83e0be60ddaab2e9c5c1
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-07-26 00:19:59 +0530

    Time: 84 ms (37.50%), Space: 43 MB (80.99%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.java
@@ -1,13 +1,28 @@
 class Solution {
     public boolean canPartition(int[] nums) {
         HashMap<Integer, Boolean> hm = new HashMap<>();
-
+        
         int sum = 0;
         for(int i=0; i<nums.length ; i++){
             sum+=nums[i];
         }
         if(sum%2 != 0) return false;
-        return targetSum(nums, 0, sum, sum, hm);
+        int[] dp = new int[sum+1];
+        Arrays.fill(dp, -1);
+        //return targetSum(nums, 0, sum, sum, hm);
+        return targetSum2(nums, 0, sum, sum, dp);
+    }
+    
+    public static boolean targetSum2(int[] arr, int idx, int curr, int sum, int[] dp){
+        if(curr == sum/2) return true;
+        if(idx>= arr.length || curr<0) return false;
+        if(dp[curr] != -1) return dp[curr] == 1? true :false;
+        boolean take = targetSum2(arr, idx+1, curr-arr[idx], sum, dp);
+        boolean donttake = targetSum2(arr, idx+1, curr, sum, dp);
+
+        boolean result = take || donttake;
+        dp[curr] = result ? 1 : 0;
+        return result;
     }
     
    public static boolean targetSum(int[] arr, int idx, int curr, int sum, HashMap<Integer, Boolean> hm){
