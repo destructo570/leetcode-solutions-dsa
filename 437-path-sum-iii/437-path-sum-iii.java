f654c20735389aa4849de1dcc043346212cb267d
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-08-02 01:14:01 +0530

    Time: 24 ms (40.53%), Space: 43.8 MB (65.77%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.java
+++ 437-path-sum-iii/437-path-sum-iii.java
@@ -13,24 +13,15 @@
  *     }
  * }
  */
-class Solution {
-    int paths=0;
-    public int pathSum(TreeNode root, int targetSum) {
-        if(root==null) return paths;
-        
-        helper(root, targetSum);
-        pathSum(root.left, targetSum);
-        pathSum(root.right, targetSum);
-        return paths;
+public class Solution {
+    public int pathSum(TreeNode root, int sum) {
+        if (root == null) return 0;
+        return pathSumFrom(root, sum) + pathSum(root.left, sum) + pathSum(root.right, sum);
     }
     
-    public void helper(TreeNode root, long target){
-        if(root==null) return;
-        if(target-root.val==0) {
-            paths++;
-        }
-        
-        helper(root.left, target-root.val);
-        helper(root.right, target-root.val);
+    private int pathSumFrom(TreeNode node, long sum) {
+        if (node == null) return 0;
+        return (node.val == sum ? 1 : 0) 
+            + pathSumFrom(node.left, sum - node.val) + pathSumFrom(node.right, sum - node.val);
     }
 }
