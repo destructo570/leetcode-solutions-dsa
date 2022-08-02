07efe33729d3bee367844c81343026235d4627a1
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-08-02 14:54:03 +0530

    Time: 1 ms (96.41%), Space: 43.3 MB (12.09%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -18,48 +18,35 @@ class Solution {
         List<List<Integer>> result = new ArrayList<>();
         Queue<TreeNode> q = new LinkedList<>();
         if(root==null) return result;
-        // boolean flag = false;
+        boolean flag = false;
         q.offer(root);
+        
         while(!q.isEmpty()){
             int size=q.size();
             List<Integer> list = new ArrayList<>();
             
             for(int i=0; i<size; i++){
                 TreeNode curr = q.poll();
-                list.add(curr.val);    
-                // if(!flag){
-                    // if(curr.right!=null) q.offer(curr.right);
-                    // if(curr.left!=null) q.offer(curr.left);
-                // }else{
-                    if(curr.left!=null) q.offer(curr.left);
-                    if(curr.right!=null) q.offer(curr.right);
-                // }
+                    
+                if(flag){
+                   list.add(0, curr.val);
+                }else{
+                   list.add(curr.val);
+                }
+
+                if(curr.left!=null) {
+                    q.offer(curr.left);
+                }
+                if(curr.right!=null) {
+                    q.offer(curr.right);
+                }
             }
             
             result.add(list);
-            
-            // flag=flag?false:true;
+            flag=!flag;
         }
-        
-         for(int i=1; i<result.size(); i+=2){
-             result.set(i, reverseArrayList(result.get(i)));
-         }
-        
-        
         return result;
     }
     
-    public List<Integer> reverseArrayList(List<Integer> alist)
-    {
-        // Arraylist for storing reversed elements
-        ArrayList<Integer> revArrayList = new ArrayList<Integer>();
-        for (int i = alist.size() - 1; i >= 0; i--) {
- 
-            // Append the elements in reverse order
-            revArrayList.add(alist.get(i));
-        }
- 
-        // Return the reversed arraylist
-        return revArrayList;
-    }
+
 }
