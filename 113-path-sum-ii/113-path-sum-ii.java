719d5db37d1d55ad0401706dc2ab099915560ac0
Vishal Kashi <dev.vishalkashi@gmail.com>
2022-08-04 12:30:44 +0530

    Time: 2 ms (84.19%), Space: 42.7 MB (93.70%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.java
+++ 113-path-sum-ii/113-path-sum-ii.java
@@ -14,26 +14,58 @@
  * }
  */
 class Solution {
-
-    public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
-        List<List<Integer>> result = new ArrayList<>();
-        if(root==null) return result;
-        
-        helper(root, new LinkedList<>(), result, targetSum);
-        return result;
-    }
     
-    public void helper(TreeNode root, LinkedList<Integer> curr, List<List<Integer>> result, int targetSum){
-
-        if(root == null) return;
-
-        curr.addLast(root.val);
-        if(targetSum-root.val == 0 && root.left==null && root.right==null){
-            result.add(new ArrayList<Integer>(curr));
-        }
-        helper(root.left, curr, result, targetSum-root.val);
-        helper(root.right, curr, result, targetSum-root.val);
-        curr.removeLast();
+        public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> res = new ArrayList<>();
+        List<Integer> path = new ArrayList<>();
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        int SUM = 0;
+        TreeNode cur = root;
+        TreeNode pre = null;
+        while(cur!=null || !stack.isEmpty()){
+            while(cur!=null){
+                stack.push(cur);
+                path.add(cur.val);
+                SUM+=cur.val;
+                cur=cur.left;
+            }
+            cur = stack.peek();
+            if(cur.right!=null && cur.right!=pre){
+                cur = cur.right;
+                continue;
+            } 
+            if(cur.left==null && cur.right==null && SUM==sum) 
+                res.add(new ArrayList<Integer>(path));
+  
+            pre = cur;
+            stack.pop();
+            path.remove(path.size()-1);
+            SUM-=cur.val;
+            cur = null;
         
+        }
+        return res;
     }
+
+//     public List<List<Integer>> pathSum(TreeNode root, int targetSum) {
+//         List<List<Integer>> result = new ArrayList<>();
+//         if(root==null) return result;
+        
+//         helper(root, new LinkedList<>(), result, targetSum);
+//         return result;
+//     }
+    
+//     public void helper(TreeNode root, LinkedList<Integer> curr, List<List<Integer>> result, int targetSum){
+
+//         if(root == null) return;
+
+//         curr.addLast(root.val);
+//         if(targetSum-root.val == 0 && root.left==null && root.right==null){
+//             result.add(new ArrayList<Integer>(curr));
+//         }
+//         helper(root.left, curr, result, targetSum-root.val);
+//         helper(root.right, curr, result, targetSum-root.val);
+//         curr.removeLast();
+        
+//     }
 }
